d9bf0532c0066fe214bcf178f04cbbdc6f491f73
Ethereal <103569762+laerehte@users.noreply.github.com>
2022-04-22 06:41:13 +0400

    Update vbware-main.lua

--- vbware-main.lua
+++ vbware-main.lua
@@ -24,5 +24,44 @@ local StaminaRegen = Groupbox1:CreateSlider("Stamina Regen", 0, 100, 0, function
 end)
 
 local LookAngles = Groupbox1:CreateButton("Look Angles", function()
-	MainScripts.ViewAngles(true)
+	local function drawLine(player)
+		local lookangle = Drawing.new("Line")
+		lookangle.Visible = false
+		lookangle.Thickness = 1
+		lookangle.Color = Color3.new(1,0,0)
+
+		local function updater()
+			local c
+			c = Offsets.RUN.RenderStepped:Connect(function()
+				if player and player.Character and player.Character:FindFirstChild("TopHitbox") and player.Character:FindFirstChild("Humanoid") and player.Character.Humanoid.Health > 0 then
+					for idx = 10, 1, -1 do
+						local rayhit, hitpos = workspace:FindPartOnRayWithIgnoreList(Ray.new(player.Character.TopHitbox.Position, player.Character.TopHitbox.CFrame.LookVector * idx), {Offsets.CAMERA, player.Character}, false, true, "")
+						local viewportpointhit, isOnScreen = Offsets.CAMERA:WorldToViewportPoint(hitpos)
+						local headpos = Offsets.CAMERA:WorldToViewportPoint(player.Character.TopHitbox.Position)
+
+						if isOnScreen then
+							lookangle.From = Vector2.new(headpos.X, headpos.Y)
+							lookangle.To = Vector2.new(viewportpointhit.X, viewportpointhit.Y)
+							lookangle.Visible = true
+							break
+						else
+							lookangle.Visible = false
+						end
+
+					end
+				end
+			end)
+			coroutine.wrap(updater)()
+		end
+	end
+
+	for i,v in next, Offsets.PLAYERS:GetPlayers() do
+		if v ~= Offsets.PLAYER then
+			coroutine.wrap(drawLine)(v)
+		end
+	end
+
+	Offsets.PLAYERS.PlayerAdded:Connect(function(v)
+		coroutine.wrap(drawLine)(v)
+	end)
 end)
