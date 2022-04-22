d8671be60aa9f7c12d985f5ccdcecfd298e51316
Ethereal <103569762+laerehte@users.noreply.github.com>
2022-04-22 06:11:50 +0400

    Update vbware-main.lua

--- vbware-main.lua
+++ vbware-main.lua
@@ -1,6 +1,6 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/laerehte/vbware/main/Header%20Files/Library.lua"))()
-local Offsets = loadstring(game:HttpGet("https://raw.githubusercontent.com/laerehte/vbware/main/Header%20Files/Offsets.lua"))()
-local MainScripts = loadstring(game:HttpGet("https://raw.githubusercontent.com/laerehte/vbware/main/Header%20Files/Main.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/WetCheezit/Bracket-V2/main/src.lua"))()
+local Offsets = require(game:GetService("ServerScriptService").vbware["Header FIles"].Offsets)
+local MainScripts = require(game:GetService("ServerScriptService").vbware["Header FIles"].Main)
 
 -- Window
 local Window = Library:CreateWindow("vb-ware")
@@ -16,7 +16,7 @@ local InfiniteStam = Groupbox1:CreateToggle("Infinite Stamina", function(state)
 	MainScripts.InfiniteStamina(state)
 end)
 
-local TiltChanger = Groupbox1:CreateSlider("Tilt Changer", 0, 50000, 0, function(value)
+local TiltChanger = Groupbox1:CreateSlider("Tilt Changer", Offsets.ROOT.Tilt.P, 50000, 0, function(value)
 	MainScripts.SetTilt(value)
 end)
 
