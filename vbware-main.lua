7bfb088640ffd6ea18b36a5e3e7c44e433255b63
Ethereal <103569762+laerehte@users.noreply.github.com>
2022-04-22 06:31:55 +0400

    Update vbware-main.lua

--- vbware-main.lua
+++ vbware-main.lua
@@ -7,20 +7,22 @@ local Window = Library:CreateWindow("vb-ware")
 
 -- Tabs
 local Tab1 = Window:CreateTab("Local")
-local Tab2 = Window:CreateTab("Misc")
-
 local Groupbox1 = Tab1:CreateGroupbox("Player", "Left")
 
 -- Groupbox 1
-local InfiniteStam = Groupbox1:CreateToggle("Infinite Stamina", function(state)
-	MainScripts.InfiniteStamina(state)
+local InfiniteStam = Groupbox1:CreateButton("Infinite Stamina", function()
+	MainScripts.InfiniteStamina(true)
 end)
 
-local TiltChanger = Groupbox1:CreateSlider("Tilt Changer", 0, 50000, Offsets.ROOT.Tilt.P, function(value)
+local TiltChanger = Groupbox1:CreateSlider("Tilt Changer", 3000, 10000, Offsets.ROOT.Tilt.P, function(value)
 	MainScripts.SetTilt(value)
 end)
 
 
 local StaminaRegen = Groupbox1:CreateSlider("Stamina Regen", 0, 100, 0, function(value)
 	MainScripts.StaminaRegen(value)
+end)
+
+local LookAngles = Groupbox1:CreateButton("Look Angles", function()
+	MainScripts.ViewAngles(true)
 end)
