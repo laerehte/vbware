51558c79014cd042993856ade99c8bd59b94e0e2
Ethereal <103569762+laerehte@users.noreply.github.com>
2022-04-22 06:07:38 +0400

    Create Main.lua

---
+++ Header Files/Main.lua
@@ -0,0 +1,55 @@
+local scripts = {}
+local Offsets = require(game:GetService("ServerScriptService").vbware["Header FIles"].Offsets)
+
+-- Infinite Stamina
+scripts.InfiniteStamina = function(State)
+	
+	if State == true then
+		
+		for i, v in pairs(getgc()) do
+			if typeof(v) == "function" then
+				if getfenv(v).script then
+					if getfenv(v).script.Name == "InputHandler" then
+						local ups = debug.getupvalues(v)
+						if ups[2] == 10 then
+							debug.setupvalue(v,2,math.huge)
+							break
+						end
+					end
+				end
+			end
+		end
+
+		
+	elseif State == false then
+		
+		for i, v in pairs(getgc()) do
+			if typeof(v) == "function" then
+				if getfenv(v).script then
+					if getfenv(v).script.Name == "InputHandler" then
+						local ups = debug.getupvalues(v)
+						if ups[2] == 10 then
+							debug.setupvalue(v,2, 10)
+							break
+						end
+					end
+				end
+			end
+		end
+
+		
+	end
+	
+end
+
+-- Stamina Regen
+scripts.StaminaRegen = function(Value)
+	Offsets.GAME_VALUES.staminaRegen.Value = Value
+end
+
+-- Tilt Changer
+scripts.SetTilt = function(Value)
+	Offsets.ROOT.Tilt.P = Value
+end 
+
+return scripts
